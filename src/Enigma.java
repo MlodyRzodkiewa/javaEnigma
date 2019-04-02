d90fefa42b4ce673b280ef1a0431d48650f10a01
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-04-02 11:43:33 +0200

    Create a structure of Enigma class, add Rot13 to enigma

--- src/Enigma.java
+++ src/Enigma.java
@@ -2,8 +2,27 @@
 
 public class Enigma {
     public static void main(String[] args) {
-        Rot13 decrypt = new Rot13();
-        decrypt.decrypt(args);
+        for(int i=0; i < args.length; i++){
+            System.out.print(args[i]);
+        }
+        if(args.length > 0){
+            if(args.length > 2){
+                // there we can add another ciphers
+                if(args[0].equals("Rot13") && args[1].equals("-d") || args[1].equals("-e")) {
+                    Rot13 decrypt = new Rot13();
+                    decrypt.decrypt(args);
+                } //else if here
+                
+                else {
+                    System.out.println("No cipher in database!");
+                }
+            } else {
+                System.out.println("Enter text to encypt or decrypt!");
+            }      
+        } else {
+            System.out.println("Enter a cipher name!");
+            System.out.println("Type -l to show available ciphers!");
+        }
       
     }
 
