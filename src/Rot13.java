4df1d92c1f3373af8898c5c70547744e01af8892
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-04-02 10:59:51 +0200

    Set static type to decrypt method

--- src/Rot13.java
+++ src/Rot13.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 
 
+
+
 public class Rot13 { 
 
-    public String decrypt(String[] args) {
+    public static String decrypt(String[] args) {
         String s = args[2];
         ArrayList<String> tmp = new ArrayList<String>();
         if (args[0] == "Rot13" && args[1] == "-e" || args[1] == "-d");{
@@ -24,8 +26,9 @@ public class Rot13 {
             for(int i=0;i<tmp.size();i++){
                 System.out.print(tmp.get(i));
             } 
-        }
+            System.out.println("");
+        } 
         return "Error";
+        }
     }
 
-}
