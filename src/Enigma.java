50e3c60a0295c07f172fef41aef9b7273205ddbf
szymonPluta <szymon0418@gmail.com>
2019-04-03 13:07:51 +0200

    Add caesarCipher and vigenereCipher ciphers to main

--- src/Enigma.java
+++ src/Enigma.java
@@ -8,11 +8,18 @@ public class Enigma {
         if(args.length > 0){
             if(args.length > 2){
                 // there we can add another ciphers
-                if(args[0].equals("Rot13") && args[1].equals("-d") || args[1].equals("-e")) {
+                if(args[0].equals("Rot13") && (args[1].equals("-d") || args[1].equals("-e"))) {
                     Rot13 decrypt = new Rot13();
                     decrypt.decrypt(args);
                 } //else if here
-                
+                else if(args[0].equals("caesar")) {
+                    caesarCipher cipher = new caesarCipher();
+                    System.out.println(cipher.encrypt(args));
+                }
+                else if(args[0].equals("vigenere")) {
+                    vigenereCipher cipher = new vigenereCipher();
+                    System.out.println(cipher.encrypt(args));
+                }
                 else {
                     System.out.println("No cipher in database!");
                 }
