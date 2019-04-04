a6e1270c3e2e9ec23c0ef402866f751022a8e25a
szymonPluta <szymon0418@gmail.com>
2019-04-04 15:02:53 +0200

    Add whitespace handling to VigenereCipher

--- src/vigenereCipher.java
+++ src/vigenereCipher.java
@@ -1,31 +1,32 @@
-public class vigenereCipher
-{
-   public static String encrypt(String[] args) 
-   {
-		String cipheredMessage = "";
+public class vigenereCipher {
+   public static String encrypt(String[] args) {
+      String cipheredMessage = "";
       String message = args[2].toUpperCase();
       String key = args[3].toUpperCase();
 
-      if (args[1].equals("-e"))
-      {
-         for (int i = 0, j = 0; i < message.length(); i++) 
-         {
+      if (args[1].equals("-e")) {
+         for (int i = 0, j = 0; i < message.length(); i++) {
             char letter = message.charAt(i);
-            cipheredMessage += (char)(((letter - 65) + (key.charAt(j)-65)) % 26 + 65);
+            if (letter != (' ')) {
+               cipheredMessage += (char) (((letter - 65) + (key.charAt(j) - 65)) % 26 + 65);
+            } else {
+               cipheredMessage += ' ';
+            }
             j = ++j % key.length();
          }
          System.out.println("Encrypted message: " + cipheredMessage);
-      }
-      else if (args[1].equals("-d"))
-      {
-         for (int i = 0, j = 0; i < message.length(); i++) 
-         {
+      } else if (args[1].equals("-d")) {
+         for (int i = 0, j = 0; i < message.length(); i++) {
             char letter = message.charAt(i);
-            cipheredMessage += (char)((letter - key.charAt(j) + 26) % 26 + 65);
+            if (letter != (' ')) {
+               cipheredMessage += (char) ((letter - key.charAt(j) + 26) % 26 + 65);
+            } else {
+               cipheredMessage += ' ';
+            }
             j = ++j % key.length();
          }
          System.out.println("Decrypted message: " + cipheredMessage);
       }
-   return "Error";
+      return "Error";
    }
 }
