548e669db49c0e673c723bc7210beff4489eb34f
szymonPluta <szymon0418@gmail.com>
2019-04-05 09:37:52 +0200

    Add output messages and clean code

--- src/atbash.java
+++ src/atbash.java
@@ -1,32 +1,24 @@
 import java.util.Scanner;
 
-
-public class atbash
-{
-    public void atbashEncryption(String message)
-    {
+public class atbash {
+    public void atbashEncryption(String message) {
         String alphabet = "ABCDEFGHIJKLMNOPRSTUWYZ";
         String reverseAlphabet = "";
 
-        for (int i = alphabet.length()-1; i > -1; i--)
-        {
+        for (int i = alphabet.length() - 1; i > -1; i--) {
             reverseAlphabet += alphabet.charAt(i);
         }
 
         message = message.toUpperCase();
 
         String encryptedText = "";
-        for (int i = 0; i < message.length(); i++)
-        {
-            if(message.charAt(i) == (char)32)
-            {
+        for (int i = 0; i < message.length(); i++) {
+            if (message.charAt(i) == (char) 32) {
                 encryptedText += " ";
             } else {
                 int count = 0;
-                for (int j = 0; j < alphabet.length(); j++)
-                {
-                    if(message.charAt(i) == alphabet.charAt(j))
-                    {
+                for (int j = 0; j < alphabet.length(); j++) {
+                    if (message.charAt(i) == alphabet.charAt(j)) {
                         encryptedText += reverseAlphabet.charAt(j);
                         break;
                     }
@@ -36,30 +28,23 @@ public class atbash
         System.out.println(" Encrypted message: " + encryptedText);
     } // method
 
-
-    public void atbashDecryption(String message)
-    {
+    public void atbashDecryption(String message) {
         String alphabet = "ABCDEFGHIJKLMNOPRSTUWYZ";
         String reverseAlphabet = "";
 
-        for (int i = alphabet.length()-1; i > -1; i--)
-        {
+        for (int i = alphabet.length() - 1; i > -1; i--) {
             reverseAlphabet += alphabet.charAt(i);
         }
         message = message.toUpperCase(); // ! nie działa.
 
         String decryptedText = "";
-        for (int i = 0; i < message.length(); i++)
-        {
-            if(message.charAt(i) == (char)32)
-            {
+        for (int i = 0; i < message.length(); i++) {
+            if (message.charAt(i) == (char) 32) {
                 decryptedText += " ";
             } else {
                 int count = 0;
-                for (int j = 0; j < reverseAlphabet.length(); j++)
-                {
-                    if(message.charAt(i) == reverseAlphabet.charAt(j))
-                    {
+                for (int j = 0; j < reverseAlphabet.length(); j++) {
+                    if (message.charAt(i) == reverseAlphabet.charAt(j)) {
                         decryptedText += alphabet.charAt(j);
                         break;
                     }
--- src/baconian.java
+++ src/baconian.java
@@ -3,12 +3,14 @@ public class baconian {
     public void baconianEncryption(String message) {
         message = message.toLowerCase();
 
-        String messageEncrypted = message.replaceAll(" ", "b").replaceAll("[aA]", "aaaaa").replaceAll("[bB]", "aaaab").replaceAll("[cC]", "aaaba")
-                .replaceAll("[dD]", "aaabb").replaceAll("[eE]", "aabaa").replaceAll("[fF]", "aabab").replaceAll("[gG]", "aabba")
-                .replaceAll("[hH]", "aabbb").replaceAll("[iI]", "abaaa").replaceAll("[jJ]", "abaab").replaceAll("[kK]", "ababa")
-                .replaceAll("[lL]", "ababb").replaceAll("[mM]", "abbaa").replaceAll("[nN]", "abbab").replaceAll("[oO]", "abbba")
-                .replaceAll("[pP]", "abbbb").replaceAll("[rR]", "baaab").replaceAll("[sS]", "baaba").replaceAll("[tT]", "baabb")
-                .replaceAll("[uU]", "babaa").replaceAll("[wW]", "babba").replaceAll("[yY]", "bbaaa").replaceAll("[zZ]", "bbaab");
+        String messageEncrypted = message.replaceAll(" ", "b").replaceAll("[aA]", "aaaaa").replaceAll("[bB]", "aaaab")
+                .replaceAll("[cC]", "aaaba").replaceAll("[dD]", "aaabb").replaceAll("[eE]", "aabaa")
+                .replaceAll("[fF]", "aabab").replaceAll("[gG]", "aabba").replaceAll("[hH]", "aabbb")
+                .replaceAll("[iI]", "abaaa").replaceAll("[jJ]", "abaab").replaceAll("[kK]", "ababa")
+                .replaceAll("[lL]", "ababb").replaceAll("[mM]", "abbaa").replaceAll("[nN]", "abbab")
+                .replaceAll("[oO]", "abbba").replaceAll("[pP]", "abbbb").replaceAll("[rR]", "baaab")
+                .replaceAll("[sS]", "baaba").replaceAll("[tT]", "baabb").replaceAll("[uU]", "babaa")
+                .replaceAll("[wW]", "babba").replaceAll("[yY]", "bbaaa").replaceAll("[zZ]", "bbaab");
 
         System.out.println("Encrypted message: " + messageEncrypted);
     }
@@ -23,14 +25,15 @@ public class baconian {
 
         for (int i = 0; i < messageEncrypted.length() / 5; i++) {
             charEcrypt = messageEncrypted.substring(start, stop);
-            output += charEcrypt.replaceAll("aaaab", " ").replaceAll("aaaaa", "a").replaceAll("aaaab", "b").replaceAll("aaaba", "c")
-                    .replaceAll("aaabb", "d").replaceAll("aabaa", "e").replaceAll("aabab", "f").replaceAll("aabba", "g")
-                    .replaceAll("aabbb", "h").replaceAll("abaaa", "i").replaceAll("abaab", "j").replaceAll("ababa", "k")
-                    .replaceAll("ababb", "l").replaceAll("abbaa", "m").replaceAll("abbab", "n").replaceAll("abbba", "o")
-                    .replaceAll("abbbb", "p").replaceAll("baaab", "r").replaceAll("baaba", "s").replaceAll("baabb", "t");
+            output += charEcrypt.replaceAll("aaaab", " ").replaceAll("aaaaa", "a").replaceAll("aaaab", "b")
+                    .replaceAll("aaaba", "c").replaceAll("aaabb", "d").replaceAll("aabaa", "e").replaceAll("aabab", "f")
+                    .replaceAll("aabba", "g").replaceAll("aabbb", "h").replaceAll("abaaa", "i").replaceAll("abaab", "j")
+                    .replaceAll("ababa", "k").replaceAll("ababb", "l").replaceAll("abbaa", "m").replaceAll("abbab", "n")
+                    .replaceAll("abbba", "o").replaceAll("abbbb", "p").replaceAll("baaab", "r").replaceAll("baaba", "s")
+                    .replaceAll("baabb", "t");
             start += 5;
             stop += 5;
         }
         System.out.println(" Decrypted message: " + output);
-    }//method
-}//class
+    }// method
+}// class
